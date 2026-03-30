165cce6be92a0f055b9cc8fdf277bb3ebf2001e4
SophiaGiguere-Broussard8179991 <smg8179991@lpssonline.com>
2026-03-30 17:47:04 +0000

    todo 3 complete

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -37,7 +37,15 @@ var init = function (window) {
 
         // TODO 3 : Call the drawCircle() function
 
+        drawCircle()
 
+        drawCircle()
+
+        drawCircle()
+
+        drawCircle()
+
+        drawCircle()
 
         // TODO 7 : Use a loop to create multiple circles
 
